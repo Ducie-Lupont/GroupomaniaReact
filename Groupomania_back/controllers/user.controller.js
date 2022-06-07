49634407b54837f559e3228b91b9f2d4fe05e2fb
Ducie-Lupont <dupont.lucie.018@gmail.com>
2022-06-07 15:55:59 +0200

    User Update&delete

--- Groupomania_back/controllers/user.controller.js
+++ Groupomania_back/controllers/user.controller.js
@@ -17,3 +17,64 @@ module.exports.userInfo = (req, res) => {
         else console.log('ID unknown : ' + err)
     }).select('-password')
 }
+
+//Updater le profil d'un utilisateur
+module.exports.updateUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+        await UserModel.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $set: {
+                    bio: req.body.bio
+                }
+            },
+            { new: true, upsert: true, setDefaultsOnInsert: true },
+            (err, docs) => {
+                if (!err) return res.send(docs)
+                if (err) return res.status(500).send({ message: err })
+            }
+        )
+    } catch (err) {
+        return
+    }
+}
+
+//Supprimer un utilisateur
+module.exports.deleteUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+        await UserModel.remove({ _id: req.params.id }).exec()
+        res.status(200).json({ message: "Utilisateur Supprimé." })
+    } catch (err) {
+        return res.status(500).json({ message: err })
+    }
+}
+
+//Follow un utilisateur
+module.exports.follow = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+        
+    } catch (err) {
+        return res.status(500).json({ message: err })
+    }
+}
+
+//Unfollow un utilisateur
+module.exports.unfollow = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+
+    } catch (err) {
+        return res.status(500).json({ message: err })
+    }
+}
