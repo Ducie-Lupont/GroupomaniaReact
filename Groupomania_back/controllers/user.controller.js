1d003a3659525e42d8db39aba5ac426735d22144
Ducie-Lupont <dupont.lucie.018@gmail.com>
2022-06-07 15:16:05 +0200

    user routes&controllers

--- Groupomania_back/controllers/user.controller.js
+++ Groupomania_back/controllers/user.controller.js
@@ -1,7 +1,19 @@
 const UserModel = require('../models/user.model')
 const ObjectID = require('mongoose').Types.ObjectId
 
+//Obtenir les données de tous les utilisateurs
 module.exports.getAllUsers = async (req, res) => {
-    const users = await UserModel.find().select()
+    const users = await UserModel.find().select('-password')
     res.status(200).json(users)
+}
+
+//Obtenir les données d'un seul utilisateur
+module.exports.userInfo = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))                               //Si l'id de la requête n'est pas valide, je m'arrête là, et je réponds avec une erreur
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    UserModel.findById(req.params.id, (err, docs) => {                  //Si l'id de la requête est valide je récupère les données de l'utilisateur concerné, sauf le mot de passe, que je dois veiller à ne jamais envoyer dans le front.
+        if (!err) res.send(docs)
+        else console.log('ID unknown : ' + err)
+    }).select('-password')
 }
