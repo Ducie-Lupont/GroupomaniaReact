90fec7ad6d17a05f07cf7db5714b702e2dce9f90
Ducie-Lupont <dupont.lucie.018@gmail.com>
2022-06-09 21:07:00 +0200

    Auth middleware

--- Groupomania_back/server.js
+++ Groupomania_back/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/user.routes')
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({ path: './config/.env' })
 require('./config/db');
+const {checkUser} = require('./middleware/auth.middleware')
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+//jwt
+app.get('*', checkUser)
 
 //routes
 app.use('/api/user', userRoutes);
